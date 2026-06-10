6052ec6c9362abff5191cb0c7458b796c16b0320
Neha <neha.rai.1155@gmail.com>
2026-06-10 23:08:40 +0530

    Basic Calculator

---
+++ Calculator.py
@@ -0,0 +1,42 @@
+print("Calculator")
+
+
+def add(a, b):
+    return a+b
+
+
+def subtract(a, b):
+    return a-b
+
+
+def multiply(a, b):
+    return a*b
+
+
+def divide(a, b):
+    if b == 0:
+        return "cannot be divisible"
+    else:
+        return a/b
+
+
+print("Select any one")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice = input("Enter the choice:")
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+if choice == "1":
+    print("Result:", add(num1, num2))
+elif choice == "2":
+    print("Result:", subtract(num1, num2))
+elif choice == "3":
+    print("Result:", multiply(num1, num2))
+elif choice == "4":
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid choice")
